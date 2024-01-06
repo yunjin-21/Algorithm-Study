29d7309b0a0dcee8d7241f89cb0162b45af28103
leeyunjin <yuncom21@ewhain.net>
2024-01-06 23:34:28 +0900

    1006

---
+++ 브루트포스/BOJ_2309.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class BOJ_2309 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int arr[] = new int[9];
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+        int a = 0, b = 0;
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    a = i;
+                    b = j;
+                    break;
+                }
+            }
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < 9; i++) {
+            if (i == a || i == b) {
+                continue;
+            }
+            bw.write(String.valueOf(arr[i]) + "\n");
+        }
+        bw.flush();
+
+    }
+
+}
