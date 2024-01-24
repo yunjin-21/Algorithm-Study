29a9738a2ceca0f3171e74e9fec5508cab3377c5
leeyunjin <yuncom21@ewhain.net>
2024-01-25 00:58:30 +0900

    0125

--- 재귀/BOJ_1182.java
+++ 재귀/BOJ_1182.java
@@ -1,3 +1,39 @@
-public class BOJ_1182 {
+import java.util.*;
 
+public class BOJ_1182 {
+    public static int s;
+    public static int n;
+    public static int arr[];
+    public static int ans = 0; // 아무것도 안뽑는 경우도 포함됨
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        s = sc.nextInt();
+        arr = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        find(0, 0);
+
+        System.out.println(ans);
+    }
+
+    public static void find(int index, int sum) {
+        if (index == n)
+            return;
+
+        if (s == sum + arr[index]) { // 이 인덱스를 포함한 값이 s랑 같으면 그냥 증가, 0인 경우를 바로 제외시킴
+            ans++;
+        }
+        // 현재 인덱스를 포함시킬경우
+        find(index + 1, sum + arr[index]);
+        // 현재 인덱스 안포함시키는경우
+        find(index + 1, sum);
+    }
 }
+// 1 2 3
+// g(0,0)
+// g(1,0)g(1,1)
+// g(2,0)g(2,1)g(2,2)g(2,3)
+// g(3,0)g(3,1)g(3,2)g(3,3)g(3,3)g(3,4)g(3,5)g(3,6)
--- 재귀/BOJ_6603.java
+++ 재귀/BOJ_6603.java
@@ -1,3 +1,29 @@
+import java.util.*;
+
 public class BOJ_6603 {
+    public static int combi[] = new int[6];
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int tc = sc.nextInt();
+            if (tc == 0) {
+                break;
+            }
+            int arr[] = new int[tc];
+            for (int i = 0; i < tc; i++) {
+                arr[i] = sc.nextInt();
+            }
+            find(0, 0);
+            System.out.println();
+
+        }
+    }
+
+    public static find(int [] arr , int tc){
+
+    }
 
 }
